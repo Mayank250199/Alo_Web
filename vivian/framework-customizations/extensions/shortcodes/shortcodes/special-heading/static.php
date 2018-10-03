b0cdee244f9984d2cde135ec32f7c049b9f4199a
Mayank Jha <mayankjha7722@gmail.com>
2018-10-03 23:56:36 +0530

    Files removed!!

--- vivian/framework-customizations/extensions/shortcodes/shortcodes/special-heading/static.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('FW')) die('Forbidden');
-
-$uri = fw_get_template_customizations_directory_uri('/extensions/shortcodes/shortcodes/special-heading');
-
-wp_enqueue_style(
-	'fw-shortcode-special-heading',
-	$uri . '/static/css/styles.css'
-);
