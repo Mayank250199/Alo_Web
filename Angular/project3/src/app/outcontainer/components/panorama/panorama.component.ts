18f38c79df030505b6e092b8e032ff345c80bd37
Govind Singh <govind.resarch0000@rediffmail.com>
2018-10-05 02:43:04 +0530

    asd

--- Angular/project3/src/app/outcontainer/components/panorama/panorama.component.ts
+++ Angular/project3/src/app/outcontainer/components/panorama/panorama.component.ts
@@ -10,30 +10,7 @@ export class PanoramaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-      lol();
+      //lol();
   }
 
-}
-function lol() {
-
-    var $panorama = $('.panorama');
-
-    var left = $panorama.offset().left;
-    var right = $panorama.offset().right;
-    var width = $panorama.width();
-
-    $('.left').click(function(e) {
-        var offset = e.pageX - left;
-        var percentage = offset / width * 100;
-
-        $panorama.css('background-position', percentage + '% 0');
-
-    });
-    $('.right').click(function(e) {
-        var offset = e.pageX - left;
-        var percentage = offset / width * 100;
-
-        $panorama.css('background-position', percentage + '% 0');
-
-    });
 }
