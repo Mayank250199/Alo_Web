f5f57393d7feeadbeb94ae25012217eeed13f394
Mayank Jha <mayankjha7722@gmail.com>
2018-10-05 03:37:56 +0530

    some has happened!!

--- Angular/project3/src/app/outcontainer/components/panorama/panorama.component.ts
+++ Angular/project3/src/app/outcontainer/components/panorama/panorama.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 declare var $:any;
 @Component({
   selector: 'app-panorama',
@@ -14,3 +15,26 @@ export class PanoramaComponent implements OnInit {
   }
 
 }
+$(function() {
+
+    var $panorama = $('.panorama');
+
+    var left = $panorama.offset().left;
+    var right = $panorama.offset().right;
+    var width = $panorama.width();
+
+    $('.left').click(function(e) {
+        var offset = e.pageX - left;
+        var percentage = offset / width * 100;
+
+        $panorama.css('background-position', percentage + '% 0');
+
+    });
+    $('.right').click(function(e) {
+        var offset = e.pageX - left;
+        var percentage = offset / width * 100;
+
+        $panorama.css('background-position', percentage + '% 0');
+
+    });
+});
